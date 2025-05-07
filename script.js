2dcff1a3fc2e79b233a2e617b95972239393e94a
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-07 11:20:53 +0200

    add: snack 2

--- script.js
+++ script.js
@@ -54,3 +54,24 @@ const somma = (num1, num2) => num1 + num2;
 const longBooks = books.filter((book) => book.pages > 300);
 const longBooksTitles = longBooks.map((book) => book.title);
 console.log(longBooksTitles);
+
+// SNACK 2 ----------------------------------------
+
+const isFullPrice = (book) => {
+  const dotIndex = book.price.indexOf('.');
+  const decimals = book.price.substring(dotIndex + 1);
+
+  return decimals == '00€';
+};
+
+const availableBooks = books.filter((book) => book.available);
+const discountedBooks = availableBooks.map((book) => {
+  const discountedPrice = parseFloat(book.price.replace('€', '') * 0.8);
+  const updatedPrice = discountedPrice.toFixed(2).toString() + '€';
+
+  return { ...book, price: updatedPrice };
+});
+console.log('discountedBooks: ', discountedBooks);
+
+const fullPricedBook = discountedBooks.find((book) => isFullPrice(book));
+console.log('full price book: ', fullPricedBook);
